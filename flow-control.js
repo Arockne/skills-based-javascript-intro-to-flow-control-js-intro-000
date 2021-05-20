2c397e5d50a314cba8c75fe2d53119f25e451310
Daniel Calvin <arockne2003@yahoo.com>
2021-05-20 18:23:07 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -5,7 +5,10 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
-  return basicTeenager(age);
+  if (age >= 13 && age <= 19) {
+    return 'You are a teenager!';
+  }
+  return 'You are not a teenager';
 }
 
 function ageChecker(age) {
